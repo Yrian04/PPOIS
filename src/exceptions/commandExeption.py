57c60a3dfa3823b49d6d2780189d24b2113139af
Ehor Hleoza <gleoza04@gmail.com>
2024-02-27 19:48:46 +0300

    realise command exception

---
+++ src/exceptions/commandExeption.py
@@ -0,0 +1,10 @@
+from src.controller.command import Command
+
+
+class CommandException(Exception):
+    def __init__(self, command: Command):
+        self.command = command
+
+    def __str__(self):
+        return (f"Command {self.command.name} with arguments "
+                f"{", ".join(self.command.args)} can't be executed")
