fb4da40c98cd0bc94ae6c37fe92f3002170d5338
Ehor Hleoza <gleoza04@gmail.com>
2024-02-27 19:49:01 +0300

    realise view

---
+++ src/view/consoleInputer.py
@@ -0,0 +1,8 @@
+from inputer import Inputer
+from src.controller.input import Input
+
+
+class ConsoleInputer(Inputer):
+    def get_input(self) -> Input:
+        command_name, *args = input("/").strip().split(" ")
+        return Input(command_name, *args)
